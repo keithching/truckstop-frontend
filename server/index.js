57efee3e1f3fa2e5bfed401e3da4ea201d593bc7
Keith <keithching91@gmail.com>
2022-07-31 18:04:39 +0900

    commented out server side migration

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 9000;
 (async () => {
   try {
     console.log("Running migrations...");
-    await db.migrate.latest();
+    // await db.migrate.latest();
 
     console.log("Starting express...");
     app.listen(PORT, () => {
