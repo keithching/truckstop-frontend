ff9b0a55d4af2fdfeaad95ceb4e259bd90c57070
jmcraineydev <jmcraineydev@gmail.com>
2022-08-05 13:30:07 +0900

    feat: add api call for city & state

--- src/utils/dataFromServer.js
+++ src/utils/dataFromServer.js
@@ -1,13 +1,23 @@
 import axios from "axios";
 
-const server = "https://truck-stop-test.herokuapp.com/";
+const server = "https://truck-stop-test.herokuapp.com/api/";
 
 const getAllLocations = () => {
-  return axios.get(`${server}api/locations`);
+  return axios.get(`${server}locations`);
+};
+
+const getLocations = (queryParams) => {
+  let searchString;
+  if (queryParams) {
+    if (queryParams.state && queryParams.city) {
+      searchString = `?state=${queryParams.state}&city=${queryParams.city}`;
+    }
+    return axios.get(`${server}locations${searchString}`);
+  }
 };
 
 const getSearchItems = () => {
-  return axios.get(`${server}api/searchItems`);
+  return axios.get(`${server}searchItems`);
 };
 
-export { getAllLocations, getSearchItems };
+export { getAllLocations, getSearchItems, getLocations };
