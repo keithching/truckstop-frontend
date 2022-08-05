11eaeca13eb670563418419df42ddaa8fbde9736
khiz125 <ken.hiro118@gmail.com>
2022-08-05 16:00:10 +0900

    feat: add Header.jsx, SelectArea.jsx and css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { CgMenu } from "react-icons/cg";
 import SelectArea from "./SelectArea";
 import "./Header.css";
 
 export default function Header() {
+  const [isShowUp, setIsShowUp] = useState(false);
+
   return (
     <header>
       <div className={"wrapper-container"}>
         <div className={"Header"}>Service finder</div>
         <CgMenu
           className={"hamburger"}
-          onClick={() => console.log("select state and city!!")}
+          onClick={() => setIsShowUp(!isShowUp)}
         />
       </div>
-      <SelectArea />
+      <SelectArea
+        className={isShowUp ? "selectArea.show" : "selectArea.hide"}
+      />
     </header>
   );
 }
