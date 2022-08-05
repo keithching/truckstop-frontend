0d4d140269861cf016fc7542329a4193f2f2daf3
jmcraineydev <jmcraineydev@gmail.com>
2022-08-05 15:59:07 +0900

    feat: add functionality to update markers on location

---
+++ src/utils/getLocationsbyUser.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import { getLocations, getAllLocations } from "./dataFromServer";
+
+const getByUserLocation = async (city, state) => {
+  const params = {};
+  if (city && state) {
+    params.city = city;
+    params.state = state;
+    return getLocations(params);
+  }
+  return getAllLocations();
+};
+
+export { getByUserLocation };
